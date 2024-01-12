6b14804348e15811848c491b3b8f147fc6eca352
kumarsumiit <summiit.kumar91@gmail.com>
2024-01-13 01:18:56 +0530

    Add html and js file

---
+++ style.js
@@ -0,0 +1,13 @@
+const mainHeading = document.getElementById('main-heading');
+mainHeading.textContent = 'Fruit World';
+mainHeading.style.color = 'orange';
+
+const header = document.getElementById('header');
+header.style.backgroundColor = 'green';
+header.style.borderBottom ='3px solid orange';
+
+const bheader = document.getElementById('basket-heading');
+bheader.style.color = 'green';
+
+const thanks = document.getElementById('thanks');
+thanks.innerHTML = '<p>Please visit us again</P>';
