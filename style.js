325e723599b0675d02631604bc8fd925966ef454
kumarsumiit <summiit.kumar91@gmail.com>
2024-01-14 12:42:32 +0530

    getElementsByTagName Method

--- style.js
+++ style.js
@@ -18,3 +18,9 @@ for(let i=0; i<fruit.length;i++){
   fruit[i].style.fontWeight = 'bold';
   fruit[2].style.backgroundColor = 'yellow'
 }
+const listItems = document.getElementsByTagName('li');
+listItems[4].style.color = 'blue';
+
+for(let i=0; i<listItems.length;i++){
+  listItems[i].style.fontStyle = 'italic';
+}
