c1d7d0a764d68c072f87aded07a0b4f5e96fd9ef
kumarsumiit <summiit.kumar91@gmail.com>
2024-01-16 23:32:33 +0530

    querySelector & querySelectorAll Methods

--- style.js
+++ style.js
@@ -24,3 +24,31 @@ listItems[4].style.color = 'blue';
 for(let i=0; i<listItems.length;i++){
   listItems[i].style.fontStyle = 'italic';
 }
+const mainHeadings = document.querySelector('#main-heading');
+mainHeadings.style.textAlign = 'right';
+
+const basketHeading = document.querySelector('#basket-heading');
+basketHeading.style.color = 'brown';
+basketHeading.style.marginLeft = '30px';
+
+const fruits = document.querySelector('.fruits');
+fruits.style.backgroundColor = 'gray';
+fruits.style.padding = '30px';
+fruits.style.margin = '30px';
+fruits.style.width = '500px';
+fruits.style.borderRadius = '5px';
+fruits.style.listStyleType = 'none';
+
+const fruitItems = document.querySelectorAll('.fruit');
+for (let i = 0; i < fruitItems.length; i++) {
+  fruitItems[i].style.backgroundColor = 'white';
+  fruitItems[i].style.padding = '10px';
+  fruitItems[i].style.margin = '10px';
+  fruitItems[i].style.borderRadius = '5px';
+}
+const evenFruitItems = document.querySelectorAll('.fruit:nth-child(even)');  // Corrected selector
+for (let i = 0; i < evenFruitItems.length; i++) {
+  evenFruitItems[i].style.backgroundColor = 'brown';
+  evenFruitItems[i].style.color = 'white';
+}
+const additionalElements = document.querySelectorAll('.additional-class');
